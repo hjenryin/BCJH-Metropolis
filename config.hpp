36d73d3b984bf7a4d9bdb002259727184bdb8d1b
hjenryin <hjenryin@outlook.com>
2023-01-24 04:47:53 +0800

    将详细模式改为命令行指令

--- config.hpp
+++ config.hpp
@@ -4,12 +4,11 @@
 #define NUM_CHEFS 3
 #define DISH_PER_CHEF 3
 #define T_MAX_CHEF 500   // 选择厨师时的初始温度
-#define T_MAX_RECIPE 400 // 对于每一组厨师，选择菜谱时的初始温度
+#define T_MAX_RECIPE 500 // 对于每一组厨师，选择菜谱时的初始温度
 #define ITER_CHEF 4000   // 选择厨师时的迭代次数
 #define ITER_RECIPE 3000 // 对于每一组厨师，选择菜谱时的迭代次数
 #define BANQUET true     // 宴会模式
 #define AVOID_CHEF_1 true
 #define AVOID_CHEF_2 true
-#define AVOID_CHEF_3 true  // 计算时不考虑三火厨师
-#define VERBOSE_MODE false // 输出详细信息
+#define AVOID_CHEF_3 true // 计算时不考虑三火厨师
 #endif
