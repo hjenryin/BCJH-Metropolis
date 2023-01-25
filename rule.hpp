4b4b02809a289de52e31bd5f2d16d37ce4e498ae
hjenryin <72179072+hjenryin@users.noreply.github.com>
2023-01-26 04:49:25 +0800

    兔年宴会5规则

--- rule.hpp
+++ rule.hpp
@@ -10,70 +10,79 @@
  * @return int : the optimal `full` value
  */
 int banquetRule(BanquetRule *const &rule, States &s) {
-    if (s.recipe[0]->cookAbility.stirfry > 0 &&
-        s.recipe[1]->cookAbility.stirfry > 0 &&
-        s.recipe[2]->cookAbility.stirfry > 0) {
+    if (s.recipe[0]->cookAbility.fry > 0 && s.recipe[1]->cookAbility.fry > 0 &&
+        s.recipe[2]->cookAbility.fry > 0) {
         for (int i = 3; i < 6; i++) {
-            rule[i].addRule.skillBuff.abilityBuff.knife += 100;
+            if (s.recipe[i]->cookAbility.bake > 0) {
+                rule[i].addRule.full += 1;
+            }
         }
     }
-    if (s.recipe[0]->cookAbility.boil > 0 &&
-        s.recipe[1]->cookAbility.boil > 0 &&
-        s.recipe[2]->cookAbility.boil > 0) {
+    if (s.recipe[0]->cookAbility.knife > 0 &&
+        s.recipe[1]->cookAbility.knife > 0 &&
+        s.recipe[2]->cookAbility.knife > 0) {
         for (int i = 3; i < 6; i++) {
-            rule[i].addRule.skillBuff.abilityBuff.steam += 100;
+            rule[i].addRule.skillBuff.abilityBuff.stirfry += 100;
         }
     }
-    rule[2].addRule.buff = -30;
     for (int i = 0; i < 3; i++) {
-        if (s.recipe[i]->flavor.salty == true) {
-            rule[i].addRule.full -= 3;
+        if (s.recipe[i]->flavor.spicy == true) {
+            rule[i].addRule.buff += 100;
             break;
         }
     }
-
-    //////
-
-    rule[3].addRule.fullAdd = false;
-    rule[3].addRule.full = 6;
-
-    rule[4].baseRule.directAdd = 500;
-
+    rule[2].add(rule[2]);
+    ////////
     for (int i = 3; i < 6; i++) {
-        if (s.recipe[i]->rarity == 3) {
-            rule[i].addRule.full -= 5;
+        if (s.recipe[i]->rarity == 2) {
+            rule[i].baseRule.buff += 50;
             break;
         }
     }
     for (int i = 3; i < 6; i++) {
-        if (s.recipe[i]->flavor.bitter == true) {
-            rule[i].addRule.buff -= 50;
-            break;
-        }
-    }
-
-    //////
-
-    for (int i = 6; i < 9; i++) {
         if (s.recipe[i]->rarity == 3) {
             rule[i].addRule.full += 3;
             break;
         }
     }
-    for (int i = 6; i < 9; i++) {
+    for (int i = 3; i < 6; i++) {
         if (s.recipe[i]->cookAbility.knife > 0) {
             rule[i].baseRule.directAdd += 500;
             break;
         }
     }
-    for (int i = 6; i < 9; i++) {
-        if (s.recipe[i]->cookAbility.stirfry > 0) {
-            rule[i].baseRule.buff -= 15;
+    for (int i = 3; i < 5; i++) {
+        if (s.recipe[i]->rarity == 4) {
+            rule[i + 1].add(rule[i + 1]);
             break;
         }
     }
-
-    rule[8].add(rule[8]);
+    ////////
+    for (int i = 6; i < 8; i++) {
+        if (s.recipe[i]->cookAbility.steam > 0 &&
+            s.recipe[i + 1]->flavor.tasty == true) {
+            rule[i].addRule.full += 8;
+            break;
+        }
+    }
+    for (int i = 6; i < 9; i++) {
+        if (s.recipe[i]->cookAbility.stirfry > 0) {
+            rule[i].addRule.buff += 150;
+            break;
+        }
+    }
+    for (int i = 6; i < 9; i++) {
+        if (s.recipe[i]->rarity == 4) {
+            rule[i].addRule.buff += 50;
+            break;
+        }
+    }
+    for (int i = 6; i < 9; i++) {
+        if (s.recipe[i]->rarity == 1) {
+            rule[i].baseRule.directAdd += 1000;
+            break;
+        }
+    }
     return 46;
 }
 
