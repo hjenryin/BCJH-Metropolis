63029be736ac8cc88c8a0d43f1c3966b2c96f5f7
ztf111 <81567653+ztf111@users.noreply.github.com>
2023-01-26 16:57:13 +0800

    Update config.hpp

--- config.hpp
+++ config.hpp
@@ -5,8 +5,8 @@
 #define DISH_PER_CHEF 3
 #define T_MAX_CHEF 20000 // 选择厨师时的初始温度
 #define T_MAX_RECIPE 20000 // 对于每一组厨师，选择菜谱时的初始温度
-#define ITER_CHEF 3000     // 选择厨师时的迭代次数
-#define ITER_RECIPE 4000 // 对于每一组厨师，选择菜谱时的迭代次数
+#define ITER_CHEF 10000     // 选择厨师时的迭代次数
+#define ITER_RECIPE 10000 // 对于每一组厨师，选择菜谱时的迭代次数
 #define BANQUET true     // 宴会模式
 #define AVOID_CHEF_1 true
 #define AVOID_CHEF_2 true
