fd8a9d560fe6273abbf8c5e972b2200aea14931a
hjenryin <72179072+hjenryin@users.noreply.github.com>
2023-01-26 08:44:08 +0800

    修复调料技能和菜品口味加成技能混淆。

--- src/bcjh.cpp
+++ src/bcjh.cpp
@@ -10,15 +10,19 @@
 #include "../config.hpp"
 #include <unistd.h>
 #include <stdio.h>
+#include <fstream>
 bool Chef::coinBuffOn = true;
 void initChefRecipePairs(CRPairs &, std::map<int, Chef> &,
                          std::map<int, Recipe> &);
+void run(CList &, RList &, CRPairs &, bool, bool);
+void calculator(CList &, RList &, CRPairs &);
 int main(int argc, char *argv[]) {
     int opt;
     bool silent = false;
     bool verbose = false;
-    const char *optstring = "sv";
+    const char *optstring = "svch";
     int seed = time(NULL);
+    bool calculate = false;
     while ((opt = getopt(argc, argv, optstring)) != -1) {
         if (opt == 's') {
             silent = true;
@@ -26,8 +30,16 @@ int main(int argc, char *argv[]) {
         if (opt == 'v') {
             verbose = true;
         }
+        if (opt == 'c') {
+            calculate = true;
+        }
+        if (opt = 'h') {
+            std::cout << "-s: 进度条逢500更新" << std::endl;
+            std::cout << "-v: 输出详细信息" << std::endl;
+            std::cout << "-c: 从文件中读取配置，计算分数" << std::endl;
+        }
     }
-    // seed = 1674723744;
+    // seed = 1674729618;
     if (true)
         std::cout << "随机种子：" << seed << std::endl;
     srand(seed);
@@ -41,7 +53,14 @@ int main(int argc, char *argv[]) {
     for (auto chef = chefList.begin(); chef != chefList.end(); chef++) {
         chef->second.loadRecipeCapable(recipeList);
     }
-
+    if (!calculate) {
+        run(chefList, recipeList, chefRecipePairs, verbose, silent);
+    } else {
+        calculator(chefList, recipeList, chefRecipePairs);
+    }
+}
+void run(CList &chefList, RList &recipeList, CRPairs &chefRecipePairs,
+         bool verbose, bool silent) {
     SARunner saRunner(&chefList, &recipeList, &chefRecipePairs, ITER_CHEF,
                       T_MAX_CHEF, 0, e::getTotalPrice, r::randomChef,
                       f::t_dist_fast);
@@ -59,7 +78,6 @@ int main(int argc, char *argv[]) {
                                r::randomRecipe, f::t_dist_fast);
         saRunnerPrint.run(s.chef, false, false, true, "../out/recipe");
     }
-    return 0;
 }
 
 void initChefRecipePairs(CRPairs &chefRecipePairs,
@@ -79,4 +97,24 @@ void initChefRecipePairs(CRPairs &chefRecipePairs,
         }
     }
     // std::cout << chefRecipePairs.size() << std::endl;
+}
+void calculator(CList &chefList, RList &recipeList, CRPairs &p) {
+    std::ifstream f;
+    f.open("../in/out.txt");
+    States s;
+    for (int i = 0; i < NUM_CHEFS; i++) {
+        int c, t;
+        f >> c >> t;
+        s.chef[i] = &chefList[c * 6 + t];
+    }
+    for (int i = 0; i < NUM_CHEFS * DISH_PER_CHEF; i++) {
+        int r;
+        f >> r;
+        s.recipe[i] = &recipeList[r];
+    }
+    f.close();
+    int score = e0::sumPrice(s, &chefList, &recipeList, &p, false, true);
+    SARunner saRunner(&chefList, &recipeList, &p, ITER_CHEF, T_MAX_CHEF, 0);
+    saRunner.print(s, true);
+    std::cout << "\n\nScore: " << score << std::endl;
 }
