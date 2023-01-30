cb511062d6ec405b6f651253a1cafa8b4fd0d7cc
leyfeeling <123666001+leyfeeling@users.noreply.github.com>
2023-01-30 22:23:50 +0800

    Update toolEquipped.hpp

--- toolEquipped.hpp
+++ toolEquipped.hpp
@@ -3,15 +3,19 @@
 #include "src/Chef.hpp"
 
 void toolEquipped(Chef *chef) {
-    return; // 以下为示例代码，可自行修改。完成后注释此行。
+//    return; // 以下为示例代码，可自行修改。完成后注释此行。
     if (chef->name == "达浪") {                   // 金锄头
         chef->skill.ability.add(30);              // 全技法+30
         chef->skill.materialBuff.vegetable += 15; // 蔬菜售价+15%
         chef->tool = NO_TOOL;                     // 不允许装备其他厨具
     }
-    if (chef->name == "宋书汐汐") {         // 幸福礼花炮
+    if (chef->name == "姒露") {         // 幸福礼花炮
         chef->skill.ability.stirfry += 150; // 炒技法+150
         chef->skill.abilityBuff.bake += 25; // 烤售价+25%
+        chef->tool = NO_TOOL;               // 不允许装备其他厨具
+    }
+        if (chef->name == "师焉") {         // 银色调味瓶
+        chef->skill.abilityBuff.bake += 10; // 烤售价+10%
         chef->tool = NO_TOOL;               // 不允许装备其他厨具
     }
     if (chef->name == "小公明") {             // 鲨鱼之吻
