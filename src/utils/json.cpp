43c2d3910e17cf9bd2413c6ec4f69217cf4f826b
gfygfygfy <123710773+gfygfygfy@users.noreply.github.com>
2023-01-30 17:29:45 +0800

    Delete src directory

--- src/utils/json.cpp
+++
@@ -1,8 +0,0 @@
-#include "json.hpp"
-int getInt(Json::Value &v) {
-    try {
-        return v.asInt();
-    } catch (Json::LogicError &e) {
-        return atoi(v.asCString());
-    }
-}
--- src/utils/json.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef UTILS_JSON_HPP
-#define UTILS_JSON_HPP
-#include "../include/json/json.h"
-int getInt(Json::Value &v);
-#endif
