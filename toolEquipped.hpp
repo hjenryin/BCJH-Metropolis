1367de61825b5e16d61a791c6265ea48a13a4c6d
hjenryin <72179072+hjenryin@users.noreply.github.com>
2023-02-01 23:47:53 +0800

    补充厨具说明

--- toolEquipped.hpp
+++ toolEquipped.hpp
@@ -1,25 +1,47 @@
 #ifndef TOOLEQUIPPED_HPP
 #define TOOLEQUIPPED_HPP
 #include "src/Chef.hpp"
+#include "src/Values.hpp"
+
+// 关于技法：
+// 例：skill.ability.add(30);     // 全技法+30
+// 例：skill.ability.bake += 150; // 烤技法+150,
+//   bake 烤；knife 切；stirfry 炒；fry 炸；steam 蒸；boil 煮
+
+// 关于技法售价：
+// 例：skill.abilityBuff.bake += 25; // 烤售价+25%
+
+// 关于口味售价：
+// 例：skill.flavorBuff.sweet += 30; // 甜售价+30%
+//   sweeet 甜；spicy 辣；salty 咸；tasty 鲜；sour 酸；bitter 苦
+
+// 关于食材售价：
+// 例：skill.materialBuff.vegetable += 15; // 蔬菜售价+15%
+//   vegetable 菜类；meat 肉类；fish 鱼类；creation 面类
+
+// 关于金币获得：
+// 例：skill.coinBuff += 30; // 金币获得+30%
 
 void toolEquipped(Chef *chef) {
+
     return; // 以下为示例代码，可自行修改。完成后注释此行。
-    if (chef->name == "达浪") {                   // 金锄头
-        chef->skill.ability.add(30);              // 全技法+30
-        chef->skill.materialBuff.vegetable += 15; // 蔬菜售价+15%
-        chef->tool = NO_TOOL;                     // 不允许装备其他厨具
-    }
-    if (chef->name == "宋书汐汐") {         // 幸福礼花炮
-        chef->skill.ability.stirfry += 150; // 炒技法+150
-        chef->skill.abilityBuff.bake += 25; // 烤售价+25%
+
+    auto skill = chef->skill; // 这句不用管，放着就好。
+
+    if (chef->name == "达浪") {             // 金锄头
+        skill.ability.add(30);              // 全技法+30
+        skill.materialBuff.vegetable += 15; // 蔬菜售价+15%
         chef->tool = NO_TOOL;               // 不允许装备其他厨具
     }
-    if (chef->name == "小公明") {             // 鲨鱼之吻
-        chef->skill.abilityBuff.knife += 40;  // 切售价+40%
-        chef->skill.ability.knife *= 0.9;     // 切技法-10%
-        chef->skill.materialBuff.fish += -15; // 鱼售价-15%
-        chef->tool = NO_TOOL;                 // 不允许装备其他厨具
+    if (chef->name == "宋书汐汐") {   // 幸福礼花炮
+        skill.ability.stirfry += 150; // 炒技法+150
+        skill.abilityBuff.bake += 25; // 烤售价+25%
+        chef->tool = NO_TOOL;         // 不允许装备其他厨具
+    }
+    if (chef->name == "柔可") {
+        skill.abilityBuff.knife += 20;
+        skill.materialBuff.creation += 15;
+        chef->tool = NO_TOOL;
     }
 }
-
 #endif
