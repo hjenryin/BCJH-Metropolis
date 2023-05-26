b0cfb4a073f5357568bc4a023c1b3504ab47b1c0
haoyun0 <74288482+haoyun0@users.noreply.github.com>
2023-05-26 17:44:08 +0800

    Update config.hpp

--- config.hpp
+++ config.hpp
@@ -4,14 +4,14 @@
 #define NUM_CHEFS 3
 #define DISH_PER_CHEF 3
 
-#define MODE 2 // 1:宴会模式; 2:限时任务
-#define TARGET_SCORE_APPROXIMATE 24000
+#define MODE 1 // 1:宴会模式; 2:限时任务
+#define TARGET_SCORE_APPROXIMATE 1000000
 // 预期达到的分数
 // 如果是限时任务，这里输分数线
 // 如果是宴会模式，不用特别准，差两倍/一半以内都可以
 
-#define ITER_CHEF 3000   // 选择厨师时的迭代次数
-#define ITER_RECIPE 4000 // 对于每一组厨师，选择菜谱时的迭代次数
+#define ITER_CHEF 5000   // 选择厨师时的迭代次数
+#define ITER_RECIPE 7000 // 对于每一组厨师，选择菜谱时的迭代次数
 
 #define AVOID_CHEF_1 true
 #define AVOID_CHEF_2 true
