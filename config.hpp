4f77c1e787652e73256ee4e6650da52f6b99b6f6
haoyun0 <847360401@qq.com>
2023-05-27 13:55:12 +0800

    测试是参数太大了，还是改出bug了

--- config.hpp
+++ config.hpp
@@ -12,8 +12,8 @@
 // 如果是限时任务，这里输分数线
 // 如果是宴会模式，不用特别准，差两倍/一半以内都可以
 
-#define ITER_CHEF 8000   // 选择厨师时的迭代次数
-#define ITER_RECIPE 10000 // 对于每一组厨师，选择菜谱时的迭代次数
+#define ITER_CHEF 3000   // 选择厨师时的迭代次数
+#define ITER_RECIPE 3000 // 对于每一组厨师，选择菜谱时的迭代次数
 
 #define AVOID_CHEF_1 true
 #define AVOID_CHEF_2 true
