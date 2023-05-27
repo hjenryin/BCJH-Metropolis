79ac374c745d6152295fbfefab63f3374ead15eb
haoyun0 <847360401@qq.com>
2023-05-27 13:43:03 +0800

    更改工作流

--- assets/checkUpdate.sh
+++ assets/checkUpdate.sh
@@ -1,10 +1,10 @@
 git pull
 python3 data/checkUpdate.py
-python3 assets/generateRule.py > src/rule_tmp.hpp
-if [ $? -eq 0 ]; then
-    mv src/rule_tmp.hpp src/banquetRule.hpp
-else
-    rm src/rule_tmp.hpp
-    echo "自动生成规则失败"
-fi
+# python3 assets/generateRule.py > src/rule_tmp.hpp
+# if [ $? -eq 0 ]; then
+#     mv src/rule_tmp.hpp src/banquetRule.hpp
+# else
+#     rm src/rule_tmp.hpp
+#     echo "自动生成规则失败"
+# fi
 
