ef86066f4d1905c8e95062226150dfa4be2c82dd
haoyun0 <847360401@qq.com>
2023-05-27 13:40:05 +0800

    调参

--- config.hpp
+++ config.hpp
@@ -7,13 +7,13 @@
 #define NUM_CHEFS (NUM_GUESTS * CHEFS_PER_GUEST)
 
 #define MODE 1 // 1:宴会模式; 2:限时任务
-#define TARGET_SCORE_APPROXIMATE 2000000
+#define TARGET_SCORE_APPROXIMATE 2400000
 // 预期达到的分数
 // 如果是限时任务，这里输分数线
 // 如果是宴会模式，不用特别准，差两倍/一半以内都可以
 
-#define ITER_CHEF 12000   // 选择厨师时的迭代次数
-#define ITER_RECIPE 12000 // 对于每一组厨师，选择菜谱时的迭代次数
+#define ITER_CHEF 8000   // 选择厨师时的迭代次数
+#define ITER_RECIPE 10000 // 对于每一组厨师，选择菜谱时的迭代次数
 
 #define AVOID_CHEF_1 true
 #define AVOID_CHEF_2 true
