900709cec232ad4575be24d224e6f862ad6e253a
jack10086 <32386417+jack10086@users.noreply.github.com>
2023-06-02 16:06:16 +0800

    Update 达浪的天青貔貅

--- toolEquipped.hpp
+++ toolEquipped.hpp
@@ -34,15 +34,10 @@ void toolEquipped(Chef *chef) {
 
     auto skill = &chef->skill; // 这句不用管，放着就好。
 
-    if (chef->name == "达浪") {              // 金锄头
-        skill->ability.add(30);              // 全技法+30
-        skill->materialBuff.vegetable += 15; // 蔬菜售价+15%
+    if (chef->name == "达浪") {              // 天青貔貅印
+        skill->ability.knife += 80;              // 
+        skill->flavorBuff.sour += 30; // 蔬菜售价+15%
         chef->tool = NO_TOOL;                // 不允许装备其他厨具
-    }
-    if (chef->name == "宋书汐汐") {    // 幸福礼花炮
-        skill->ability.stirfry += 150; // 炒技法+150
-        skill->abilityBuff.bake += 25; // 烤售价+25%
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
     }
 }
 #endif
