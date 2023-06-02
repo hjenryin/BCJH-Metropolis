bb7c7c685fe60d5dda81dfebeee1050008b1f893
haoyun0 <847360401@qq.com>
2023-06-02 10:18:54 +0800

    update: 扩展兼容多贵客

--- config.hpp
+++ config.hpp
@@ -1,17 +1,19 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-#define NUM_CHEFS 3
+#define NUM_GUESTS 2
 #define DISH_PER_CHEF 3
+#define CHEFS_PER_GUEST 3
+#define NUM_CHEFS (NUM_GUESTS * CHEFS_PER_GUEST)
 
-#define MODE 2 // 1:宴会模式; 2:限时任务
-#define TARGET_SCORE_APPROXIMATE 24000
+#define MODE 1 // 1:宴会模式; 2:限时任务
+#define TARGET_SCORE_APPROXIMATE 2400000
 // 预期达到的分数
 // 如果是限时任务，这里输分数线
 // 如果是宴会模式，不用特别准，差两倍/一半以内都可以
 
-#define ITER_CHEF 3000   // 选择厨师时的迭代次数
-#define ITER_RECIPE 4000 // 对于每一组厨师，选择菜谱时的迭代次数
+#define ITER_CHEF 10000   // 选择厨师时的迭代次数
+#define ITER_RECIPE 10000 // 对于每一组厨师，选择菜谱时的迭代次数
 
 #define AVOID_CHEF_1 true
 #define AVOID_CHEF_2 true
@@ -21,7 +23,7 @@
 // ** 以下内容不建议修改，除非你清楚地知道这是什么意思 ** //
 // ************************************************** //
 
-#define T_MAX_CHEF TARGET_SCORE_APPROXIMATE / 50 // 选择厨师时的初始温度
+#define T_MAX_CHEF TARGET_SCORE_APPROXIMATE / 100 // 选择厨师时的初始温度
 #define T_MAX_RECIPE                                                           \
     TARGET_SCORE_APPROXIMATE / 50 // 对于每一组厨师，选择菜谱时的初始温度
 
