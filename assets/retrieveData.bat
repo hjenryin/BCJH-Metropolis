822d0d8009f9e7c004fb3f066c4c08a90ed914a5
hjenryin <72179072+hjenryin@users.noreply.github.com>
2023-06-17 09:00:45 +0800

    retrieveData

---
+++ assets/retrieveData.bat
@@ -0,0 +1,20 @@
+@echo off
+echo 修改配置、了解更多请至https://github.com/hjenryin/BCJH-Metropolis/ 以便在线运行
+echo 有疑问请至https://github.com/hjenryin/BCJH-Metropolis/discussions/categories/q-a 提问
+echo 有任何想法可至https://github.com/hjenryin/BCJH-Metropolis/discussions/categories/general 讨论
+set "bcjhid="
+set /p bcjhid=如果需要重新下载用户配置，请输入白菜菊花id；否则直接回车：
+
+if "%bcjhid%"=="" (
+    echo 未输入id，跳过下载用户配置
+) 
+
+if not "%bcjhid%"=="" (
+    echo 正在下载用户配置
+    powershell -command $json=$(iwr -uri "https://bcjh.xyz/api/download_data?id=%bcjhid%" ^| ConvertFrom-Json^) ^; if ($json.result -eq 'True'^) { Write-Host 用户名（中文可能乱码）：$($json.user^) -ForegroundColor:Green^; echo $json.data ^> userData.json } else { Write-Host 下载失败，请检查id是否正确 -BackgroundColor:Red -ForegroundColor:White ^}
+
+)
+
+.\bcjh.exe
+echo 按任意键关闭
+pause
