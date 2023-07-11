a40671545051bb32150f1d3e39cd0777e272fc4f
hjenryin <72179072+hjenryin@users.noreply.github.com>
2023-07-11 12:30:46 +0800

    Fix bug in removeSystemCall.

--- assets/removeSystemCall.py
+++ assets/removeSystemCall.py
@@ -1,10 +1,11 @@
 import os
-fl=os.listdir("..")
-for f in fl:
+dir=".."
+fl=os.listdir(dir)
+for f in fl:    
     if f.endswith(".hpp"):
-        with open(f,"r",encoding="utf-8") as f1:
+        with open(dir+f,"r",encoding="utf-8") as f1:
             lines=f1.readlines()
-        with open(f,"w",encoding="utf-8") as f2:
+        with open(dir+f,"w",encoding="utf-8") as f2:
             for line in lines:
                 if line.find("#define VIS_HISTORY")!=-1:
                     continue
